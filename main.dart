f29fcfa82950d4189a2d92bb16226f8a5d3bf7bd
chiiima <139869388+chiiima@users.noreply.github.com>
2023-12-17 13:03:27 +0100

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,34 +1,27 @@
-mport 'package:addicfree_app/pages/HomePage.dart';
-import 'package:addicfree_app/pages/MainScreen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:addicfree_app/firebase_options.dart';
+import 'package:addicfree_app/pages/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-// decides if the user has already an accout or it's his first time using the app
-// if he's already logged in ,the MainScreen will be displayed
-// if not , the login page will be displayed
-
-
-
-class WrapperPage extends StatelessWidget {
-  const WrapperPage({Key? key}) : super(key: key);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder <User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context , snapshot){
-          //user logged in
-          if (snapshot.hasData){
-            return MainScreen();
-          }
-          // user not logged in
-        else {
-          return  const HomePage();
-          }
-        },
-      )
-    );
+    return const  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WrapperPage(), // decides either the login page or the MainScreen will be displayed based on the user's activity
+      );
   }
 }
+
+
 
